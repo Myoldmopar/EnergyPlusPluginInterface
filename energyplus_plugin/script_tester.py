ccf79313ad4f4282b55a96cb310238d967ada639
Edwin Lee <leeed2001@gmail.com>
2018-12-07 08:43:34 -0600

    Clean up test script now

--- energyplus_plugin/script_tester.py
+++ energyplus_plugin/script_tester.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 """
-This file is a standalone PyEMS script tester.
-It is called using a single argument, the path to a PyEMS file
+This file is a standalone EnergyPlusPlugin script tester.
+It is called using a single argument, the path to an EnergyPlus plugin file
 """
 
 import inspect
@@ -11,10 +11,10 @@ import sys
 from importlib import util as import_util
 
 
-class PyEMSTesting(object):
+class EnergyPlusPluginTesting(object):
 
     @staticmethod
-    def py_ems_file_tester(file_path):
+    def plugin_file_tester(file_path):
         modules = []
 
         if os.path.exists(file_path):
@@ -29,23 +29,23 @@ class PyEMSTesting(object):
             print("ERROR : File path does NOT end with .py")
             return 1
 
-        module_spec = import_util.spec_from_file_location('py_ems_module', file_path)
+        module_spec = import_util.spec_from_file_location('eplus_plugin_module', file_path)
         this_module = import_util.module_from_spec(module_spec)
         try:
             modules.append(this_module)
             module_spec.loader.exec_module(this_module)
             print("   OK : Python import process completed successfully!")
         except ImportError as ie:
-            # this error generally means they have a bad py_ems class or something
-            print("ERROR : Import error occurred on py_ems file %s: %s" % (file_path, str(ie)))
+            # this error generally means they have a bad plugin class or something
+            print("ERROR : Import error occurred on plugin file %s: %s" % (file_path, str(ie)))
             return 1
         except SyntaxError as se:
             # syntax errors are, well, syntax errors in the Python code itself
-            print("ERROR : Syntax error occurred on py_ems file %s, line %s: %s" % (file_path, se.lineno, se.msg))
+            print("ERROR : Syntax error occurred on plugin file %s, line %s: %s" % (file_path, se.lineno, se.msg))
             return 1
         except Exception as e:
-            # there's always the potential of some other unforeseen thing going on when a py_ems is executed
-            print("ERROR : Unexpected error occurred trying to import py_ems: %s: %a" % file_path, str(e))
+            # there's always the potential of some other unforeseen thing going on when a plugin is executed
+            print("ERROR : Unexpected error occurred trying to import plugin: %s: %a" % file_path, str(e))
             return 1
 
         successful_classes = []
@@ -55,24 +55,24 @@ class PyEMSTesting(object):
                 this_class_name, this_class_type = this_class
                 print(" INFO : Encountered class: \"" + this_class_name + "\", testing now...")
                 # so right here, we could check is_subclass, but this would also match the base class, which
-                # is imported in each PyEMS class.  No need to do that.  For now I'm going to check the direct
+                # is imported in each plugin file.  No need to do that.  For now I'm going to check the direct
                 # parent class of this class to verify we only get direct descendants.  We can evaluate this later.
-                # if is_subclass(this_class_type, EMSInterface):
+                # if is_subclass(this_class_type, EnergyPlusPlugin):
                 num_inheritance = len(this_class_type.__bases__)
                 base_class_name = this_class_type.__bases__[0].__name__
-                py_ems_base_class_name = 'EMSInterface'
-                if num_inheritance == 1 and py_ems_base_class_name in base_class_name:
+                plugin_base_class_name = 'EnergyPlusPlugin'
+                if num_inheritance == 1 and plugin_base_class_name in base_class_name:
                     print("   OK : Basic inheritance checks out OK for class: " + this_class_name)
 
                     try:
-                        py_ems_instance = this_class_type()
+                        plugin_instance = this_class_type()
                         print("   OK : Instantiation of derived class works")
                     except Exception as e:
                         print("ERROR : Instantiation of derived class malfunctioning; reason: " + str(e))
                         return 1
 
                     try:
-                        this_calling_point = py_ems_instance.get_calling_point()
+                        this_calling_point = plugin_instance.get_calling_point()
                         print("   OK : Overridden get_calling_point() function execution works")
                     except Exception as e:
                         print("ERROR : get_calling_point() function not overridden, or is broken; reason: " + str(e))
@@ -81,11 +81,11 @@ class PyEMSTesting(object):
                     if isinstance(this_calling_point, int):
                         print("   OK : Calling point comes back as an integer, this is the expected condition")
                     else:
-                        print("ERROR : Bad get_calling_point return; must be int from EMSInterface.CallingPointsMirror")
+                        print("ERROR : Bad get_calling_point return; must be int from CallingPointsMirror class")
                         return 1
 
                     try:
-                        sensors = py_ems_instance.get_sensed_data_list()
+                        sensors = plugin_instance.get_sensed_data_list()
                         print("   OK : Overridden get_sensed_data_list() function execution works")
                     except Exception as e:
                         print("ERROR : get_sensed_data_list() function not overridden, or is broken; reason: " + str(e))
@@ -108,7 +108,7 @@ class PyEMSTesting(object):
                         return 1
 
                     try:
-                        actuators = py_ems_instance.get_actuator_list()
+                        actuators = plugin_instance.get_actuator_list()
                         print("   OK : Overridden get_actuator_list() function execution works")
                     except Exception as e:
                         print(
@@ -134,16 +134,32 @@ class PyEMSTesting(object):
                     # now we need to instantiate the sensor dictionary by calling the base class method for each sensor
                     try:
                         for sensor in sensors:
-                            py_ems_instance.update_sensed_datum(sensor, 0.0)
+                            plugin_instance.update_sensed_datum(sensor, 0.0)
                     except Exception as e:
                         print("ERROR : Could not call update_sensed_data - this method should not be overridden!")
                         print("       Error message for diagnostics: " + str(e))
 
                     try:
-                        py_ems_instance.ems_main()
-                        print("   OK : Overridden ems_main() function execution works")
+                        response = plugin_instance.main()
+                        print("   OK : Overridden main() function execution works")
                     except Exception as e:
-                        print("ERROR : ems_main() function not overridden, or is broken; reason: " + str(e))
+                        print("ERROR : main() function not overridden, or is broken; reason: " + str(e))
+                        return 1
+
+                    if(isinstance(response, list):
+                        print("   OK : main() returns a list, this is the expected condition")
+                    else:
+                        print("ERROR : Bad return from main(); it must return a list of floats!")
+                        return 1
+
+                    bad_actuated_value = False
+                    for i, val in enumerate(response):
+                        if isinstance(val, float):
+                            print("   OK : argument #%s from main is a float" % i)
+                        else:
+                            bad_actuated_value = True
+                            print("ERROR : argument #%s from main must be a float!" % i)
+                    if bad_actuated_value:
                         return 1
 
                     successful_classes.append(this_class_name)
@@ -153,22 +169,22 @@ class PyEMSTesting(object):
                     continue
 
         if len(successful_classes) > 0:
-            print("   OK : Found %s successful py_ems imports:" % len(successful_classes))
+            print("   OK : Found %s successful EnergyPlusPlugin classes:" % len(successful_classes))
             for c in successful_classes:
                 print("   OK :   " + c)
             return 0
         else:
-            print("ERROR : Did not find ANY successful py_ems imports in this file!")
+            print("ERROR : Did not find ANY successful EnergyPlusPlugin imports in this file!")
             return 1
 
 
 def main():
     if len(sys.argv) != 2:
-        print("Bad call to tester, give one command line argument, the full path to a py_ems file")
+        print("Bad call to tester, give one command line argument, the full path to an EnergyPlus plugin file")
         sys.exit(2)
     else:
         arg_file_path = sys.argv[1]
-        response = PyEMSTesting.py_ems_file_tester(arg_file_path)
+        response = EnergyPlusPluginTesting.plugin_file_tester(arg_file_path)
         sys.exit(response)
 
 
