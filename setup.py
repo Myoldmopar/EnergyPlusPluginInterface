21e11013d332c18993031e89660063ac6f7402e4
Edwin Lee <leeed2001@gmail.com>
2018-12-07 08:27:54 -0600

    Rename again

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import codecs
 import os
 import setuptools
 
-from epps import version
+from energyplus_plugin import version
 
 
 this_dir = os.path.abspath(os.path.dirname(__file__))
@@ -12,9 +12,9 @@ with codecs.open(os.path.join(this_dir, 'README.rst'), encoding='utf-8') as i_fi
     long_description = i_file.read()
 
 setuptools.setup(
-    name='energyplus_epps',
+    name='energyplus_plugin',
     version=version,
-    description='EnergyPlus Python Scripting Interface',
+    description='EnergyPlus Python Plugin Interface',
     long_description=long_description,
     url='https://github.com/nrel/energyplus',
     author='DOE',
@@ -33,6 +33,6 @@ setuptools.setup(
     install_requires=[],
     extras_require={},
     entry_points = {
-        'console_scripts': ['test_script=epps.test_epps:main']
+        'console_scripts': ['test_script=energyplus_plugin.script_tester:main']
     }
 )
